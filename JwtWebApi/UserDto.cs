3f4c746f29441322948e75aee96e4eaf2e4c019d
Siddharth Bansari <s.bansari6131@gmail.com>
2022-03-06 22:09:13 +0530

    Add project files.

---
+++ JwtWebApi/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApi
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
